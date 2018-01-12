2da55f86c92a861cd1952097852997e6dee2b2eb
ewonguoft <eltong.wong@mail.utoronto.ca>
2018-01-12 13:48:20 -0500

    checking tests

---
+++ __tests__/splash-test.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import Splash from '../src/components/Login/Splash';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = 1;
+  expect(tree).toBe(1);
+});
